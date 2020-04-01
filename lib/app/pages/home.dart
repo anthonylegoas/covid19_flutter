7ce0e9c639883202951eb53fb7e556dee77d1fea
Anthony Le Goas <anthony.legoas@gmail.com>
2020-04-01 10:49:06 +0200

    ♻ (app) make HomePage stateless

--- lib/app/pages/home.dart
+++ lib/app/pages/home.dart
@@ -2,47 +2,16 @@ import 'package:covid19_flutter/app/pages/stats/stats_container.dart';
 import 'package:covid19_flutter/cdk/appbar/rounded_appbar.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  String _selectedCountry;
-
-  @override
-  void initState() {
-    super.initState();
-    // @todo Implement country selection.
-    _selectedCountry = 'France';
-  }
-
-  void _refreshData() {
-    setState(() {
-      // @todo Implement refresh.
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: RoundedAppBar(
-        title: widget.title,
+        title: 'COVID-19 Stats',
       ),
       backgroundColor: Theme.of(context).primaryColor.withOpacity(.7),
       body: Center(
-        child: StatsContainer(
-          country: _selectedCountry,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _refreshData,
-        tooltip: 'Refresh',
-        child: Icon(Icons.refresh),
+        child: StatsContainer(),
       ),
     );
   }
