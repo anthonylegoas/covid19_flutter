15d3dbf5d142dcd949fd6d856806a1f4f67557c2
Anthony Le Goas <anthony.legoas@gmail.com>
2020-04-01 10:49:44 +0200

    ♻ (app) make Stats use CovidBloc

--- lib/app/pages/stats/stats_container.dart
+++ lib/app/pages/stats/stats_container.dart
@@ -1,56 +1,30 @@
-import 'dart:convert';
 import 'package:covid19_flutter/app/pages/stats/stats.dart';
-import 'package:covid19_flutter/models/covid_stats.dart';
+import 'package:covid19_flutter/blocs/blocs.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class StatsContainer extends StatefulWidget {
-  StatsContainer({Key key, this.country}) : super(key: key);
-
-  final String country;
-
-  @override
-  _StatsContainerState createState() => _StatsContainerState();
-}
-
-class _StatsContainerState extends State<StatsContainer> {
-  Future<CovidStats> _covidStats;
-
-  @override
-  void initState() {
-    super.initState();
-    _covidStats = fetchData();
-  }
-
-  Future<CovidStats> fetchData() async {
-    final response = await http.get('https://api.covid19api.com/summary');
-
-    if (response.statusCode == 200) {
-      // @todo find a better API 💩
-      return json
-          .decode(response.body)['Countries']
-          .map((d) => CovidStats.fromJson(d))
-          .singleWhere((d) => d.country == widget.country);
-    } else {
-      throw Exception('Failed to load COVID-19 data');
-    }
-  }
-
+class StatsContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<CovidStats>(
-      future: _covidStats,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return Stats(covidStats: snapshot.data);
-        } else if (snapshot.hasError) {
+    return BlocBuilder<CovidBloc, CovidState>(
+      builder: (context, state) {
+        if (state is CovidStatsEmpty) {
+          BlocProvider.of<CovidBloc>(context)
+              .add(FetchCovidStats(country: 'France'));
+          return Center(child: CircularProgressIndicator());
+        }
+        if (state is CovidStatsLoading) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (state is CovidStatsLoaded) {
+          return Stats(covidStats: state.covidStats);
+        }
+        if (state is CovidStatsError) {
           return Text(
-            "${snapshot.error}",
-            style: TextStyle(color: Colors.white),
+            'Something went wrong!',
+            style: TextStyle(color: Colors.red),
           );
         }
-
-        return CircularProgressIndicator();
       },
     );
   }
